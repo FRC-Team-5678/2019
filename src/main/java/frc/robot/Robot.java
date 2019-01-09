e87a21031458be99c983394cdcc9e2496524926a
Andrewl5678 <35178785+Andrewl5678@users.noreply.github.com>
2019-01-09 10:52:17 -0800

    sets up fully funcitnal limelight

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.networktables.NetworkTable;
@@ -25,11 +26,13 @@ public class Robot extends TimedRobot {
 NetworkTableEntry tx = table.getEntry("tx");
 NetworkTableEntry ty = table.getEntry("ty");
 NetworkTableEntry ta = table.getEntry("ta");
+NetworkTableEntry tv = table.getEntry("tv");
 
 
 
 
 //read values periodically
+double v = tv.getDouble(0.0);
 double x = tx.getDouble(0.0);
 double y = ty.getDouble(0.0);
 double area = ta.getDouble(0.0);
@@ -40,15 +43,15 @@ double area = ta.getDouble(0.0);
 //SmartDashboard.putNumber("LimelightArea", area);
   
   //motor setup
-  Spark left = new Spark(1);
-  Spark right = new Spark(2);
+  Spark left = new Spark(0);
+  Spark right = new Spark(1);
   
   //drive setup
   DifferentialDrive myRobot = new DifferentialDrive(left,right);
  
  // Joystick setup
-  Joystick main = new Joystick(1);
-  double speedX = main.getRawAxis(2);  //change to whats needed
+  Joystick main = new Joystick(0);
+  double speedX = main.getRawAxis(0);  //change to whats needed
   double rotatZ = main.getRawAxis(1);  //change to whats needed
   
  
@@ -68,11 +71,50 @@ double area = ta.getDouble(0.0);
 
   @Override
   public void teleopInit() {
+   
   }
 
   @Override
   public void teleopPeriodic() {
-    myRobot.arcadeDrive(speedX, rotatZ);
+    double v = tv.getDouble(0.0);
+
+      if(v == 0){
+        left.set(.3);
+        
+      }
+      else{
+        double xOffset = tx.getDouble(0.0);
+        double yOffset = ty.getDouble(0.0);
+        
+        if(Math.abs(xOffset)>6){
+          System.out.println("0.02");
+          System.out.println(0.02 * xOffset);
+          left.set(0.03 * xOffset);
+          right.set(0.03* xOffset);
+        }
+        else if(Math.abs(xOffset)>3){
+          System.out.println("0.05");
+          System.out.println(0.15 * xOffset);
+          left.set(0.07 * xOffset);
+          right.set(0.07 * xOffset);
+        }
+        else{
+          if(main.getTriggerPressed()){
+            myRobot.arcadeDrive(.2, 0);
+            Timer.delay(1);
+          }
+        }
+      }
+   
+   
+   
+    /*if(main.getTrigger()){
+    double xOffset = tx.getDouble(0.0);
+    left.set(.3 * xOffset);
+    right.set(.3* xOffset);
+    }*/
+  
+
   }
 
   @Override
@@ -81,6 +123,12 @@ double area = ta.getDouble(0.0);
 
   @Override
   public void testPeriodic() {
+  //   double xOffset = tx.getDouble(0.0);
+  //  System.out.println(xOffset);
+  //  System.out.println(Math.abs(xOffset));
+   
+  double yOffset = ty.getDouble(0.0);
+  System.out.println((26-21.5)/Math.tan(5+yOffset));
   }
 
 }
